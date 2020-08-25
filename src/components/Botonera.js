21b9d860f332c5788e1d0d67405c070c41664067
Jorge Fidel Prieto Linares <jorgefprietol@gmail.com>
2020-08-25 16:32:32 -0500

    botones y caja

---
+++ src/components/Botonera.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Botonera.css';
+
+function Botonera() {
+  return (
+    <div className="Botonera">
+        <button>Rojo</button>
+        <button>Verde</button>
+    </div>
+  );
+}
+
+export default Botonera;
---
+++ src/components/Caja.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Caja.css';
+
+function Caja() {
+  return (
+    <div className="Caja">
+        <div className='color-container'></div>
+    </div>
+  );
+}
+
+export default Caja;
